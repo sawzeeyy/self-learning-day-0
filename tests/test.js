b9a29752626085b136fb09b4d98e183dcab10661
Shuaib Oladigbolu <saoladigbolu@gmail.com>
2017-09-26 00:03:15 +0100

    Chore: Updated test cases

--- tests/test.js
+++ tests/test.js
@@ -16,6 +16,26 @@ describe("AritGeo", () => {
 		it("should return '-1' for [1,2]", () =>  {
 			assert.equal(AritGeo([1,2]), '-1');
 		});
+
+		it("should return 'Arithmetic' for [1,3,5,7,9]", () =>  {
+			assert.equal(AritGeo([1,3,5,7,9]), 'Arithmetic');
+		});
+
+		it("should return 'Geometric' for [2,4,8,16,32]", () =>  {
+			assert.equal(AritGeo([2,4,8,16,32]), 'Geometric');
+		});
+
+		it("should return '0' for []", () =>  {
+			assert.equal(AritGeo([]), '0');
+		});
+
+		it("should return 'Arithmetic' for [9,8,7,6,5]", () =>  {
+			assert.equal(AritGeo([9,8,7,6,5]), 'Arithmetic');
+		});
+
+		it("should return 'Geometric' for [64,32,16,8,4]", () =>  {
+			assert.equal(AritGeo([64,32,16,8,4]), 'Geometric');
+		});
 	})
 
 	describe("handle invalid input", () => {
@@ -34,6 +54,22 @@ describe("AritGeo", () => {
 
 		it("should return '-1' for [81,72,27,9,3]",  () => {
 			assert.equal(AritGeo([81,72,27,9,3]), '-1');
+		});
+
+		it("should return 'undefined' for oneMillionWords",  () => {
+			assert.equal(AritGeo('oneMillionWords'), 'undefined');
+		});
+
+		it("should return '-1' for [2,4,8,10,12,16]", () =>  {
+			assert.equal(AritGeo([2,4,8,10,12,16]), '-1');
+		});
+
+		it("should return '-1' for [2]", () => {
+			assert.equal(AritGeo([2]), '-1');
+		});
+
+		it("should return '-1' for [1,2,3,6]", () =>  {
+			assert.equal(AritGeo([1,2,3,6]), '-1');
 		});
 	})
 })
